845317351c3eabb433a71e4f3e788acb8d3a8d24
Pawel <gamer.4ply@gmail.com>
2023-12-13 21:49:30 +0200

    Wire up GetUsers API call to /log-event command (proof of concept)

--- src/main/kotlin/org/trackedout/AgroNet.kt
+++ src/main/kotlin/org/trackedout/AgroNet.kt
@@ -8,7 +8,12 @@ import net.minecraft.command.argument.EntityArgumentType
 import net.minecraft.server.command.CommandManager.argument
 import net.minecraft.server.command.CommandManager.literal
 import net.minecraft.text.Text
+import okhttp3.OkHttpClient
 import org.slf4j.LoggerFactory
+import org.trackedout.client.apis.UsersApi
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
+
 
 object AgroNet : ModInitializer {
     private val logger = LoggerFactory.getLogger("agronet")
@@ -17,6 +22,13 @@ object AgroNet : ModInitializer {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
+
+        val usersApi = UsersApi(
+            client = OkHttpClient.Builder()
+                .connectTimeout(5.seconds.toJavaDuration())
+                .callTimeout(30.seconds.toJavaDuration())
+                .build()
+        )
 
         CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
             dispatcher.register(literal("log-event")
@@ -32,14 +44,25 @@ object AgroNet : ModInitializer {
                                     .executes { context ->
                                         val event = StringArgumentType.getString(context, "event")
                                         val player = EntityArgumentType.getPlayer(context, "player")
-                                        val count = IntegerArgumentType.getInteger(context, "count");
+                                        val count = IntegerArgumentType.getInteger(context, "count")
 
                                         context.source.sendFeedback(
                                             { Text.literal("Processing /log-event { event=${event}, player=${player.name}, count=${count} }") },
                                             true
                                         )
 
-
+                                        try {
+                                            val someUser = usersApi.usersGet().results?.get(0)?.name
+                                            context.source.sendFeedback(
+                                                { Text.literal("Some user: $someUser") },
+                                                true
+                                            )
+                                        } catch (e: Exception) {
+                                            context.source.sendFeedback(
+                                                { Text.literal("Failed to call Users API: ${e.message}") },
+                                                true
+                                            )
+                                        }
 
                                         1
                                     })
