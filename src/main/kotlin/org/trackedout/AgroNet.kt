e7e6e58a703fd51fdbc0c51622b0eeeabbbc9660
Pawel <gamer.4ply@gmail.com>
2023-12-16 07:27:01 +0200

    WIP code to teleport a player into the the dungeon when they strike the door with a key
    Now, when a player attacks a block Agro-net checks whether it's a Dungeon door block (we'll use NBT tags for that in the future) and if so, it does some checks like whether they have a shard and shulker box. If they do, it removes the shulker box and TPs them to some test location.
    
    In the future this will call out to DungaDunga so that it can call back to the velocity server (via RCON) to move the player around.

--- src/main/kotlin/org/trackedout/AgroNet.kt
+++ src/main/kotlin/org/trackedout/AgroNet.kt
@@ -6,13 +6,24 @@ import com.mojang.brigadier.arguments.StringArgumentType
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback
+import net.minecraft.block.Block
+import net.minecraft.block.BlockState
 import net.minecraft.command.argument.EntityArgumentType
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
 import net.minecraft.nbt.StringNbtReader
 import net.minecraft.server.command.CommandManager.argument
 import net.minecraft.server.command.CommandManager.literal
+import net.minecraft.server.command.CommandOutput
+import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.server.world.ServerWorld
+import net.minecraft.sound.SoundEvents
 import net.minecraft.text.Text
+import net.minecraft.util.ActionResult
+import net.minecraft.util.Formatting
+import net.minecraft.util.math.Vec2f
 import okhttp3.OkHttpClient
 import org.slf4j.LoggerFactory
 import org.trackedout.client.apis.UsersApi
@@ -22,6 +33,19 @@ import kotlin.time.toJavaDuration
 
 object AgroNet : ModInitializer {
     private val logger = LoggerFactory.getLogger("Agro-net")
+
+    private fun isDeckedOutShulker(it: ItemStack) = it.item.name == Items.SHULKER_BOX.name
+    private fun isDeckedOutKey(it: ItemStack) = it.item.name == Items.ECHO_SHARD.name
+
+    private fun isDeckedOutDoor(state: BlockState) = state.isOf(Block.getBlockFromItem(Items.BLACKSTONE))
+
+    private fun PlayerEntity.isReadyToStartDungeonRun(): Boolean {
+        return hasShulkerInInventory() && this.hasKeyInHand()
+    }
+
+    private fun PlayerEntity.hasShulkerInInventory() = this.inventory.containsAny(::isDeckedOutShulker)
+
+    private fun PlayerEntity.hasKeyInHand(): Boolean = this.handItems.any(::isDeckedOutKey)
 
     override fun onInitialize() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -37,23 +61,24 @@ object AgroNet : ModInitializer {
 
         ServerTickEvents.END_SERVER_TICK.register(ServerTickListener())
 
+        AttackBlockCallback.EVENT.register { player, world, hand, pos, direction ->
+            val state = world.getBlockState(pos)
+            if (!isDeckedOutDoor(state)) {
+                return@register ActionResult.PASS
+            }
+
+            return@register attemptToEnterDungeon(player)
+        }
+
         CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
-            dispatcher.register(literal("gief-shulker")
+            dispatcher.register(literal("enter-dungeon")
                 .requires { it.hasPermissionLevel(2) } // Command Blocks have permission level of 2
                 .executes { context ->
-                    if (context.source.player != null) {
-                        logger.info("Is player")
-                        val inventory = context.source.player!!.inventory
-
-                        val shulkerBox = ItemStack(Items.SHULKER_BOX)
-                        // TODO: Fetch this data from DungaDunga
-                        shulkerBox.nbt =
-                            StringNbtReader.parse("{BlockEntityTag:{Items:[{Count:1b,Slot:0b,id:\"minecraft:redstone\"},{Count:1b,Slot:1b,id:\"minecraft:chest\"},{Count:1b,Slot:2b,id:\"minecraft:slime_block\"},{Count:1b,Slot:3b,id:\"minecraft:redstone_torch\"},{Count:1b,Slot:4b,id:\"minecraft:redstone_block\"}],id:\"minecraft:shulker_box\"}}")
-//                        shulkerBox.nbt[BlockItem.BLOCK_ENTITY_TAG_KEY].toString() // FYI that's the tag for *just* the blocks inside the shulker
-                        inventory.insertStack(shulkerBox)
-                        inventory.updateItems()
+                    val player = context.source.player
+                    if (player != null) {
+                        attemptToEnterDungeon(player)
                     } else {
-                        logger.info("Not player, meh")
+                        logger.warn("Attempting to enter dungeon but command is not run as a player, ignoring...")
                     }
 
                     1
@@ -64,32 +89,26 @@ object AgroNet : ModInitializer {
             dispatcher.register(literal("take-shulker")
                 .requires { it.hasPermissionLevel(2) } // Command Blocks have permission level of 2
                 .executes { context ->
-                    if (context.source.player != null) {
-                        logger.info("Is player")
-                        val player = context.source.player!!
-                        val inventory = player.inventory
-                        inventory.remove(
-                            { item ->
-                                if (item.item.name == Items.SHULKER_BOX.name) {
-                                    logger.info("Removing shulker!")
-                                    val nbt = item.nbt
-                                    if (nbt != null) {
-                                        logger.info("NBT data: ${nbt.asString()}")
-                                    } else {
-                                        logger.warn("NBT data not present!")
-                                    }
-                                    true
-                                } else {
-                                    false
-                                }
-                            },
-                            -1,
-                            player.playerScreenHandler.craftingInput
-                        )
-
-                        inventory.updateItems()
+                    val player = context.source.player
+                    if (player != null) {
+                        takeShulkerFromPlayer(player)
                     } else {
-                        logger.info("Not player, meh")
+                        logger.warn("Attempting to take shulker but command is not run as a player, ignoring...")
+                    }
+
+                    1
+                })
+        }
+
+        CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
+            dispatcher.register(literal("gief-shulker")
+                .requires { it.hasPermissionLevel(2) } // Command Blocks have permission level of 2
+                .executes { context ->
+                    val player = context.source.player
+                    if (player != null) {
+                        giveShulkerToPlayer(player)
+                    } else {
+                        logger.warn("Attempting to give shulker but command is not run as a player, ignoring...")
                     }
 
                     1
@@ -118,11 +137,17 @@ object AgroNet : ModInitializer {
                                         )
 
                                         try {
-                                            val someUser = usersApi.usersGet().results?.get(0)?.name
-                                            context.source.sendFeedback(
-                                                { Text.literal("Some user: $someUser") },
-                                                true
-                                            )
+//                                            val someUser = eventsApi.eventsPost(
+//                                                EventsPostRequest(
+//                                                    name = event,
+//                                                    player = player,
+//                                                )
+//                                            )
+
+//                                            context.source.sendFeedback(
+//                                                { Text.literal("Some user: $someUser") },
+//                                                true
+//                                            )
                                         } catch (e: Exception) {
                                             context.source.sendFeedback(
                                                 { Text.literal("Failed to call Users API: ${e.message}") },
@@ -137,6 +162,89 @@ object AgroNet : ModInitializer {
             )
         }
 
-        logger.info("Hello Fabric world!")
+        logger.info("Agro-net online. Flee with extra flee!")
+    }
+
+    private fun attemptToEnterDungeon(player: PlayerEntity): ActionResult {
+        if (player.isSpectator) {
+            player.sendMessage(Text.literal("Sorry, spectators may not enter the dungeon this way").copy().formatted(Formatting.RED))
+            logger.warn("Spectator attempting to enter dungeon, rejecting!")
+            return ActionResult.FAIL
+        }
+
+        if (player.isCreative) {
+            return ActionResult.PASS
+        }
+
+        if (!player.isReadyToStartDungeonRun()) {
+            if (!player.hasKeyInHand()) {
+                player.sendMessage(Text.literal("You need to be holding a key to enter the Dungeon!").copy().formatted(Formatting.RED))
+                logger.info("${player.name} smacked the door without holding a key, silly player.")
+            } else if (!player.hasShulkerInInventory()) {
+                player.sendMessage(
+                    Text.literal("You need your deck (Shulker Box) in your inventory to enter the Dungeon!")
+                        .copy().formatted(Formatting.RED)
+                )
+                logger.info("${player.name} smacked the door without their Shulker Box, silly player.")
+            }
+            return ActionResult.FAIL
+        }
+
+        // The player is ready to enter the dungeon!
+        player.sendMessage(Text.literal("Entering dungeon, good luck!").copy().formatted(Formatting.BLUE))
+        logger.info("${player.name} is entering the dungeon!")
+        takeShulkerFromPlayer(player)
+
+        val commandSource = ServerCommandSource(
+            CommandOutput.DUMMY,
+            player.pos,
+            Vec2f.ZERO,
+            player.world as ServerWorld,
+            2,
+            player.name.toString(),
+            player.displayName,
+            player.world.server,
+            player
+        )
+        player.server!!.commandManager.executeWithPrefix(commandSource, "/tp 14 137 136")
+        player.playSound(SoundEvents.ENTITY_WARDEN_EMERGE, player.soundCategory, 1.0f, 1.0f)
+
+        return ActionResult.SUCCESS
+    }
+
+    private fun takeShulkerFromPlayer(player: PlayerEntity) {
+        player.inventory.remove(
+            { item ->
+                if (isDeckedOutShulker(item)) {
+                    logger.info("Removing shulker!")
+                    val nbt = item.nbt
+                    if (nbt != null) {
+                        logger.info("NBT data: ${nbt.asString()}")
+                    } else {
+                        logger.warn("NBT data not present!")
+                    }
+                    true
+                } else {
+                    false
+                }
+            },
+            -1,
+            player.playerScreenHandler.craftingInput
+        )
+
+        player.inventory.updateItems()
+    }
+
+    private fun giveShulkerToPlayer(player: PlayerEntity) {
+        // TODO: Validate that the player does not already have their shulker
+        val shulkerBox = ItemStack(Items.SHULKER_BOX)
+        // TODO: Fetch this data from DungaDunga
+        shulkerBox.nbt =
+            StringNbtReader.parse("{BlockEntityTag:{Items:[{Count:1b,Slot:0b,id:\"minecraft:redstone\"},{Count:1b,Slot:1b,id:\"minecraft:chest\"},{Count:1b,Slot:2b,id:\"minecraft:slime_block\"},{Count:1b,Slot:3b,id:\"minecraft:redstone_torch\"},{Count:1b,Slot:4b,id:\"minecraft:redstone_block\"}],id:\"minecraft:shulker_box\"}}")
+        //                        shulkerBox.nbt[BlockItem.BLOCK_ENTITY_TAG_KEY].toString() // FYI that's the tag for *just* the blocks inside the shulker
+
+        val inventory = player.inventory
+        inventory.insertStack(shulkerBox)
+        inventory.updateItems()
     }
 }
