1ec5349a9e1641b9828fe7f99ab2aed34b6197e9
CaseyMichael <caseymichaelpeters@gmail.com>
2024-01-07 15:04:59 -0700

    Remove command to enter dungeon, this is handled by dunga dunga via an event

--- src/main/kotlin/org/trackedout/AgroNet.kt
+++ src/main/kotlin/org/trackedout/AgroNet.kt
@@ -63,31 +63,6 @@ object AgroNet : ModInitializer {
         val removeDeckFromPlayerInventoryAction = RemoveDeckFromPlayerInventoryAction()
 
 
-        AttackBlockCallback.EVENT.register { player, world, hand, pos, direction ->
-            val state = world.getBlockState(pos)
-            if (!state.isDeckedOutDoor()) {
-                return@register ActionResult.PASS
-            }
-
-            return@register attemptToEnterDungeon(player)
-        }
-
-        CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
-            dispatcher.register(literal("enter-dungeon")
-                .requires { it.hasPermissionLevel(2) } // Command Blocks have permission level of 2
-                .executes { context ->
-                    val player = context.source.player
-                    if (player != null) {
-                        attemptToEnterDungeon(player)
-                    } else {
-                        logger.warn("Attempting to enter dungeon but command is not run as a player, ignoring...")
-                    }
-
-                    1
-                }
-            )
-        }
-
         CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
             dispatcher.register(literal("take-shulker")
                 .requires { it.hasPermissionLevel(2) } // Command Blocks have permission level of 2
@@ -173,53 +148,5 @@ object AgroNet : ModInitializer {
             value = default
         }
         return value
-    }
-
-    //TODO Refactor to an action
-    private fun attemptToEnterDungeon(player: PlayerEntity): ActionResult {
-        if (player.isSpectator) {
-            player.sendMessage("Sorry, spectators may not enter the dungeon this way", Formatting.RED)
-            logger.warn("Spectator attempting to enter dungeon, rejecting!")
-            return ActionResult.FAIL
-        }
-
-        if (player.isCreative) {
-            return ActionResult.PASS
-        }
-
-        if (!player.isReadyToStartDungeonRun()) {
-            if (!player.hasKeyInHand()) {
-                player.sendMessage("You need to be holding a key to enter the Dungeon!", Formatting.RED)
-                logger.info("${player.name.string} smacked the door without holding a key, silly player.")
-            } else if (!player.hasShulkerInInventory()) {
-                player.sendMessage("You need your deck (Shulker Box) in your inventory to enter the Dungeon!", Formatting.RED)
-                logger.info("${player.name.string} smacked the door without their Shulker Box, silly player.")
-            }
-            return ActionResult.FAIL
-        }
-
-        // The player is ready to enter the dungeon!
-        player.sendMessage("Entering dungeon, good luck!", Formatting.BLUE)
-        logger.info("${player.name.string} is entering the dungeon!")
-
-        val removeDeckFromPlayerInventoryAction = RemoveDeckFromPlayerInventoryAction()
-        removeDeckFromPlayerInventoryAction.execute(player)
-
-        val commandSource = ServerCommandSource(
-            CommandOutput.DUMMY,
-            player.pos,
-            Vec2f.ZERO,
-            player.world as ServerWorld,
-            2,
-            player.name.string,
-            player.displayName,
-            player.world.server,
-            player
-        )
-        player.server!!.commandManager.executeWithPrefix(commandSource, "/tp 14 137 136")
-        player.server!!.commandManager.executeWithPrefix(commandSource, "/proxycommand \"send ${player.name.string} lobby\"")
-        player.playSound(SoundEvents.ENTITY_WARDEN_EMERGE, player.soundCategory, 1.0f, 1.0f)
-
-        return ActionResult.SUCCESS
     }
 }
